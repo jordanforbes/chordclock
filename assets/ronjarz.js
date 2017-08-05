f1b697b0e259176ca8a6720c9046e8f7ccd85505
jordanforbes <Jordforb@gmail.com>
2017-08-05 17:29:28 -0400

    lists new scale

--- assets/ronjarz.js
+++ assets/ronjarz.js
@@ -1,29 +1,13 @@
 var notes = ["a","as","b","c","cs","d","ds","e","f","fs","g","gs"]
 
-var notesPicked= []
-
-// function test(){
-// 	var rand = Math.floor(Math.random()*11)
-// 	selNote= notes[rand]
-// // 	return selNote
-// 	if(selNote.match(`[a-z][s]`)){
-// 		return selNote[0]+"#"
-// 	}else{
-// 		return selNote
-// 	}
-// }
-//
-// console.log(test())
-
+var indexNotesPicked= []
+var notesPicked = []
 
 function test(max){
 
-  function randNum(max){
-
-    return Math.floor(Math.random()*max)
-
-
-  }
+    function randNum(max){
+      return Math.floor(Math.random()*max)
+    }
 
   var rand = randNum(max)
     // console.log(rand)
@@ -31,13 +15,15 @@ function test(max){
     // console.log($.inArray(rand,notesPicked))
     // if($.inArray(rand,notesPicked) == -1){console.log("unique")}else{console.log("used")}
 
-  notesPicked.push(rand)
+  indexNotesPicked.push(rand)
 
 	selNote= notes[rand]
-console.log(rand)
+  console.log(rand)
   console.log(selNote)
+  notesPicked.push(selNote)
   console.log(notes)
   notes.splice(rand,1)
+  console.log("remaining")
   console.log(notes)
 	return "#" + selNote
 //  	return selNote
@@ -51,20 +37,32 @@ console.log(rand)
 
 function randNote(note){
 	// $(".note").css("background-color","white")
-	$(""+note).css("background-color","red")
+  if( notesPicked.length<=3){
+    $(""+note).css("background-color","red")
+  }else{
+    $(""+note).css("background-color","pink")
+
+  }
 }
 
 function clear(){
   $(".note").css("background-color","white")
   notes = ["a","as","b","c","cs","d","ds","e","f","fs","g","gs"]
   notesPicked= []
+  indexNotesPicked = []
+  $(".newscale").text(notesPicked)
+
 }
 
 $("document").ready(function(){
 
     $("#randomize").click(function(){
       randNote(test(notes.length))
-      console.log(notesPicked)
+          console.log("notes selected")
+          console.log(notesPicked)
+
+      $(".newscale").text(notesPicked)
+
     })
     $("#clear").click(function(){
       clear()
