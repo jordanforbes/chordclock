6b57aa16d851c837c0856aded938f7e8ed95fda4
jordanforbes <Jordforb@gmail.com>
2017-08-05 16:25:51 -0400

    changed functions

--- assets/ronjarz.js
+++ assets/ronjarz.js
@@ -1,10 +1,41 @@
 var notes = ["a","as","b","c","cs","d","ds","e","fs","g","gs"]
 
-function test(){
-	var rand = Math.floor(Math.random()*11)
-    $(".note").css("background-color","red")
-	return notes[rand]
+// function test(){
+// 	var rand = Math.floor(Math.random()*11)
+// 	selNote= notes[rand]
+// // 	return selNote
+// 	if(selNote.match(`[a-z][s]`)){
+// 		return selNote[0]+"#"
+// 	}else{
+// 		return selNote
+// 	}
+// }
+//
+// console.log(test())
+
+
+function test(max){
+	var rand = Math.floor(Math.random()*max)
+	selNote= notes[rand]
+  console.log(selNote)
+	return "#" + selNote
+//  	return selNote
+// 	if(selNote.match(`[a-z][s]`)){
+// 		return selNote[0].toUpperCase() +"#"
+// 	}else{
+// 		return selNote[0].toUpperCase()
+// 	}
 
 }
 
-console.log(test())
+function randNote(note){
+	$(".note").css("background-color","white")
+	$(""+note).css("background-color","red")
+}
+
+$("document").ready(function(){
+    $("#randomize").click(function(){
+      randNote(test(notes.length))
+    })
+  }
+)
